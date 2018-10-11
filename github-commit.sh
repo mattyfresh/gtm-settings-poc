33cda9fc5a29b42aa5f2444f5bf73074d3e76b4c
Slack Bot! <matthew.padich@gmail.com>
2018-10-11 16:13:35 -0400

    update bash script

--- github-commit.sh
+++ github-commit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPO="https://github.com/mattyfresh/gtm-settings-poc"
+REPO_URL="https://github.com/mattyfresh/gtm-settings-poc"
 BRANCH_NAME="$1"
 
 # Config
@@ -11,7 +11,7 @@ git config user.name "Slack Bot!"
 rm -rf gtm-settings-poc
 
 # Clone Repo
-git clone "$REPO.git"
+git clone "$REPO_URL.git"
 
 # Copy Config File to repo
 cp gtm-config.json ./gtm-settings-poc/gtm-config.json
@@ -20,11 +20,11 @@ cp gtm-config.json ./gtm-settings-poc/gtm-config.json
 cd gtm-settings-poc/
 git checkout -b $BRANCH_NAME
 git add gtm-config.json
-git commit -m "Update via Slack"
+git commit -m "update to gtm config via Slack"
 
 # Push quietly to prevent showing the token in log
 git push -q https://${GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/mattyfresh/gtm-settings-poc.git $BRANCH_NAME
 
 # Echo out link to create new PR
-# @NB '@@@'' is for easy text parsing for output in Slack
-echo "@@@$REPO/pull/new/$BRANCH_NAME@@@"
+# @NB '@@@' is for easy text parsing for output in Slack
+echo "@@@$REPO_URL/pull/new/$BRANCH_NAME@@@"
